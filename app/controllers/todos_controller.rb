52b61be965bc43462b2b2f24b71a354de1499bec
riyasunny <riya@qburst.com>
2017-02-03 14:27:22 +0530

    create

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,23 +3,27 @@ class TodosController < ApplicationController
   def index
     @todos = Todo.all  
   end
-    
+  
+  
+
   def create
     @todo = Todo.create(todo_params)
     todos = Todo.all
     respond_to do |format|
       format.json { 
-        render json: {:results => todos}
-        puts json: {:results => todos}
+      render json: {:results => todos}
+      puts json: {:results => todos}
       }
     end
   end
 
   def destroy
     todo = Todo.find(params[:id])
+    #todo.destroy
     respond_to do |format|
       if todo.destroy
         format.html { redirect_to root_path }
+        #format.json { render action: 'index', status: :created, location: todo }
       end
     end
   end
